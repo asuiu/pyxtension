aa994b4fab716db7564f8562a4bfcf0733581ced
ASU <andrei.suiu@gmail.com>
2015-12-01 18:11:46 +0200

    Added Setup script

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding:utf-8
+# Author: ASU --<andrei.suiu@gmail.com>
+# Purpose: 
+# Created: 12/1/2015
+
+__author__ = 'ASU'
+
+from distutils.core import setup
+
+setup(name='pyxtension',
+      version='1.0',
+      description='Python Utilities',
+      author='Andrei Suiu',
+      author_email='andrei.suiu@gmail.com',
+      url='https://github.com/asuiu/pyxtension',
+      packages=['pyxtension'],
+      classifiers=(
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.2",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Programming Language :: Python :: Implementation :: PyPy",
+      ),
+      )
