f752ba59f227ce49ef98b9a0f5e0b71e964229dd
ASU <andrei.suiu@gmail.com>
2020-04-12 17:24:25 +0300

    Added validation()

--- py3/pyxtension/__init__.py
+++ py3/pyxtension/__init__.py
@@ -1,1 +1,14 @@
 __author__ = 'asuiu'
+
+
+class ValidateError(ValueError):
+    def __init__(self, args):
+        ValueError.__init__(self, args)
+
+
+def validate(expr, msg="Invalid argument", exc: Exception = ValidateError):
+    """
+    If the expression val does not evaluate to True, then raise a ValidationError with msg
+    """
+    if not expr:
+        raise exc(msg)
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from shutil import copy, rmtree
 __author__ = 'ASU'
 
 # Bump up this version
-VERSION = '1.13.3'
+VERSION = '1.13.4'
 
 from setuptools import setup
 from setuptools.command.install import install
