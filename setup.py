ab193c29cef488f64d9ba4b64d397d1a994d7d04
ASU <andrei.suiu@gmail.com>
2023-05-06 16:25:48 +0300

    v1.16.1 version bug due to some build issues with prev version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from os.path import join
 __author__ = 'ASU'
 
 # Bump up this version
-VERSION = '1.16.0'
+VERSION = '1.16.1'
 
 from setuptools import setup
 from setuptools.command.install import install
